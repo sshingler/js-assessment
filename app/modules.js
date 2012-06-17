4ac6618f026a23beef0c10ba43e7ac211c8056c2
Steven Shingler <shingler@gmail.com>
2012-06-17 10:42:26 +0100

    Modules passing.

--- app/modules.js
+++ app/modules.js
@@ -2,8 +2,15 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    createModule : function() {
-
+    createModule : function(greeting, name) {
+      var C = function(greeting, name){
+        this.greeting = greeting;
+        this.name = name;
+      }
+      C.prototype.sayIt = function(){
+        return this.greeting + ", " + this.name;
+      }
+      return new C(greeting, name);
     }
   };
 });
